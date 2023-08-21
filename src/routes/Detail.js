b56b6ecfff1a2efbfd276d0ffd2db756bad6eb47
Greenlucy <susan4070@gmail.com>
2023-08-21 16:30:33 +0900

    first make of Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,50 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Link, Router, Routes } from "react-router-dom";
+
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState();
+  const [wait, setWait] = useState(true);
+  console.log("id > " + id);
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
+
+    // 변수에 저장.
+    setMovie(json.data.movie);
+    setWait((current) => !current);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+
+  //   id, coverImg, title, summary, genres
+  // 비동기이므로 loading 동안 기다려줄 수 있는 시간이 필요함.
+  return (
+    <div>
+      {wait ? (
+        <h1>Please wait a moment..</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <h2>{movie.title}</h2>
+          <div>
+            <span>runtime : {movie.runtime}</span>
+            <span>language : {movie.language}</span>
+            <span>rate : {movie.rating}</span>
+          </div>
+          <p>{movie.description_full}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,12 +9,21 @@ function Home() {
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
       )
     ).json();
+    console.log(json);
     setMovies(json.data.movies);
     setLoading(false);
   };
   useEffect(() => {
     getMovies();
   }, []);
+
+  const getSuggestions = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_suggestions.json?movie_id=10`)
+    ).json();
+    console.log("getSUggestion : ", json);
+  };
+
   return (
     <div>
       {loading ? (
